c2f613940208fbec75dc5f861552760ccbb545ae
Ajeet Singh <ajeet.singh@iwgplc.com>
2021-03-22 09:40:21 +0530

    Add project files.

---
+++ bd.API/temo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BatchProcess objBatchProcess = new BatchProcess("0", 2, 2);
+            dynamic o = objBatchProcess.ProcessBatch("100", 2, 1);
+        }
+    }
+}
